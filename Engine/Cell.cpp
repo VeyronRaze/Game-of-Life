3ffabda55aacc5995350ba3059f10496fbda6dc7
VeyronRaze <neeeee502@gmail.com>
2020-12-01 10:58:18 +0200

    Added cell and Location class

--- Engine/Cell.cpp
+++ Engine/Cell.cpp
@@ -1,1 +1,19 @@
 #include "Cell.h"
+
+Cell::Cell(Location loc)
+	:
+	loc(loc)
+{}
+
+void Cell::Draw(Graphics &gfx){
+	gfx.DrawSquareWithPadding(loc.x*10, loc.y*10, 10, 10, Colors::Green);
+}
+
+void Cell::SetLoc(Location &src)
+{
+	loc = src;
+}
+
+Location Cell::GetLoc(){
+	return loc;
+}
--- Engine/Cell.h
+++ Engine/Cell.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Graphics.h"
+#include "Location.h"
 
 class Cell{
 public:
+	Cell(Location loc);
 	void Tick();
-	void Draw();
+	void Draw(Graphics &gfx);
+	void SetLoc(Location &src);
+	Location GetLoc();
 private:
+	Location loc;
 };
 
